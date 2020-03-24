6701f6f2c00db104e8d00d2c805e800e5a154594
Schampen <hampus.mann@elev.ga.ntig.se>
2020-03-24 10:52:37 +0100

    Börjat med MVC

--- src/MVCSolo.java
+++ src/MVCSolo.java
@@ -1,2 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MVCSolo {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://"+DatabaseLoginData.DBURL + ":" + DatabaseLoginData.port + "/" + DatabaseLoginData.DBname +
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    DatabaseLoginData.user, DatabaseLoginData.password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/SoloView.java
+++ src/SoloView.java
@@ -1,2 +1,29 @@
-public class SoloView {
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class SoloView extends JFrame{
+
+    private JLabel story = new JLabel();
+    private JLabel what = new JLabel("What do you do");
+    private JButton choice1 = new JButton();
+    private JButton choice2 = new JButton();
+    private JButton choice3 = new JButton();
+
+    SoloView() {
+
+        JPanel gamePanel = new JPanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,600);
+
+        gamePanel.add(story);
+        gamePanel.add(what);
+        gamePanel.add(choice1);
+        gamePanel.add(choice2);
+        gamePanel.add(choice3);
+
+        this.add(gamePanel);
+    }
+
+    
 }
