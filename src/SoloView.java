1ec093f06ecd2e1216218ea7f1e8bf86e06687b1
Schampen <hampus.mann@elev.ga.ntig.se>
2020-03-31 09:34:23 +0200

    Jobbat med MVC

--- src/SoloView.java
+++ src/SoloView.java
@@ -1,29 +1,34 @@
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
-public class SoloView extends JFrame{
-
-    private JLabel story = new JLabel();
-    private JLabel what = new JLabel("What do you do");
-    private JButton choice1 = new JButton();
-    private JButton choice2 = new JButton();
-    private JButton choice3 = new JButton();
+public class SoloView extends JFrame {
+    private JTextField story = new JTextField(10);
+    private JLabel label = new JLabel("-");
+    private JTextField choices = new JTextField(10);
+    private JButton calculateButton = new JButton("Calculate");
+    private JTextField calcSolution = new JTextField(10);
 
     SoloView() {
-
-        JPanel gamePanel = new JPanel();
-
+        // Sets up the view and adds the components
+        JPanel soloPanel = new JPanel();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800,600);
-
-        gamePanel.add(story);
-        gamePanel.add(what);
-        gamePanel.add(choice1);
-        gamePanel.add(choice2);
-        gamePanel.add(choice3);
-
-        this.add(gamePanel);
+        this.setSize(600, 200);
+        soloPanel.add(story);
+        soloPanel.add(label);
+        soloPanel.add(choices);
+        soloPanel.add(calculateButton);
+        soloPanel.add(calcSolution);
+        this.add(soloPanel);
+        // End of setting up the components --------
     }
 
+    // If the calculateButton is clicked execute a method
+    // in the Controller named actionPerformed
+    void addCalculateListener(ActionListener listenForCalcButton) {
+        calculateButton.addActionListener(listenForCalcButton);
+    }
 
+    // Open a popup that contains the error message passed
+    void displayErrorMessage(String errorMessage) {
+        JOptionPane.showMessageDialog(this, errorMessage);
+    }
 }
