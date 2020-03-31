8a2b25ef22b8d8fea8926c1557c024211381efdd
Schampen <hampus.mann@elev.ga.ntig.se>
2020-03-31 10:58:17 +0200

    Jobbat med MVC

--- src/SoloModel.java
+++ src/SoloModel.java
@@ -1,3 +1,30 @@
-public class SoloModel {
+import java.sql.*;
 
+public class SoloModel {
+    Connection conn;
+
+    {
+        try {
+            conn = DriverManager.getConnection(
+                        "jdbc:mysql://"+DatabaseLoginData.DBURL + ":" + DatabaseLoginData.port + "/" + DatabaseLoginData.DBname +
+                                "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                        DatabaseLoginData.user, DatabaseLoginData.password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    String getStory(int id) {
+        String body = "error";
+        try {
+            Statement stmt = conn.createStatement();
+            String strSelect = "select body from story where id = " + id;
+            ResultSet rset = stmt.executeQuery(strSelect);
+            body = rset.getString("body");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return body;
+    }
 }
--- src/SoloView.java
+++ src/SoloView.java
@@ -4,8 +4,8 @@ public class SoloView extends JFrame {
     private JTextField story = new JTextField(10);
     private JLabel label = new JLabel("-");
     private JTextField choices = new JTextField(10);
-    private JButton calculateButton = new JButton("Calculate");
-    private JTextField calcSolution = new JTextField(10);
+    private JButton goButton = new JButton("Go");
+    private JTextField choice = new JTextField(10);
 
     SoloView() {
         // Sets up the view and adds the components
@@ -15,16 +15,24 @@ public class SoloView extends JFrame {
         soloPanel.add(story);
         soloPanel.add(label);
         soloPanel.add(choices);
-        soloPanel.add(calculateButton);
-        soloPanel.add(calcSolution);
+        soloPanel.add(choice);
+        soloPanel.add(goButton);
         this.add(soloPanel);
         // End of setting up the components --------
     }
 
+    void changeStory(String storyBody) {
+        this.story.setText(storyBody);
+    }
+
+    void changeChoices(String choices) {
+        this.choices.setText(choices);
+    }
+
     // If the calculateButton is clicked execute a method
     // in the Controller named actionPerformed
-    void addCalculateListener(ActionListener listenForCalcButton) {
-        calculateButton.addActionListener(listenForCalcButton);
+    void addCalculateListener(ActionListener listenForGoButton) {
+        goButton.addActionListener(listenForGoButton);
     }
 
     // Open a popup that contains the error message passed
